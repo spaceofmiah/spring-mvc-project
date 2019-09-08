25b5e26799deca4f232ce73538fda17f044d9397
spaceofmiah <geek_200@outlook.com>
2019-09-08 02:05:29 +0100

    Initial commit

---
+++ src/main/java/com/springmvcproject/concertio/CoreController.java
@@ -0,0 +1,23 @@
+
+package com.springmvcproject.concertio;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author spaceofmiah
+ */
+
+@Controller
+public class CoreController {
+    
+    /*
+        *
+            this conroller will be used to handle routes and pages
+            that do not include forms processing  e.g
+            
+            about us page, faq page e.t.c
+        *
+    */
+    
+}
