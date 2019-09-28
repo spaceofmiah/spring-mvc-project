0a87f216b8523e8b783cd6f173ec45e82ffd5f65
spaceofmiah <geek_200@outlook.com>
2019-09-28 22:50:55 +0100

    Create a minimal version of AdminService & ServiceImpl

---
+++ src/main/java/com/springmvcproject/concertio/services/AdminService.java
@@ -0,0 +1,11 @@
+package com.springmvcproject.concertio.services;
+
+/**
+ * An admin service which will be able to do every things
+ * other services can perform 
+ * @author spaceofmiah
+ *
+ */
+public interface AdminService extends HallService {
+
+}
--- src/main/java/com/springmvcproject/concertio/services/HallService.java
+++ src/main/java/com/springmvcproject/concertio/services/HallService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.springmvcproject.concertio.models.Hall;
 
 
-
 public interface HallService {
 	List<Hall> findAll();
 	Hall findByLocation(String location);
