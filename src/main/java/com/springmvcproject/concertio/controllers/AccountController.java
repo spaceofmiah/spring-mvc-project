660ee32580e4c8f391f0ea730a5764c3ac0eb1ca
spaceofmiah <geek_200@outlook.com>
2019-10-11 21:49:55 +0100

    Include field filtering and compute rendering logic

--- src/main/java/com/springmvcproject/concertio/controllers/AccountController.java
+++ src/main/java/com/springmvcproject/concertio/controllers/AccountController.java
@@ -1,7 +1,14 @@
 package com.springmvcproject.concertio.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,15 +18,44 @@ import com.springmvcproject.concertio.formbeans.AccountCreationForm;
 @RequestMapping("/account")
 public class AccountController {
 	
-	@RequestMapping(value="create", method=RequestMethod.GET)
-	public String getRegistrationForm(Model model) {
-		model.addAttribute("account", new AccountCreationForm());
-		return "accountRegistration";
-	}
+	private static final String VN_REG_FORM = "accountRegistration";
+	private static final String VN_REG_OK = "redirect:registration_ok";
 	
-	@RequestMapping(value="", method=RequestMethod.POST)
-	public String postRegistrationForm(AccountCreationForm form) {
-		return "redirect:registration_ok";
+	/**
+	 * defines the field that HTTP parameters will be bound
+	 * to and ignore unwanted form parameters.
+	 * @param binder
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.setAllowedFields(
+			"firstName", "middleName", "lastName", "email", "password", 
+			"confirmPassword", "acceptTerms");
 	}
 
+	@RequestMapping(value = "create", method = RequestMethod.GET)
+	public String getRegistrationForm(Model model) {
+		model.addAttribute("account", new AccountCreationForm());
+		return VN_REG_FORM;
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public String postRegistrationForm(
+			@ModelAttribute("account") @Valid AccountCreationForm form,
+			BindingResult result) {
+		
+		convertPasswordError(result);
+		return (result.hasErrors() ? VN_REG_FORM : VN_REG_OK);
+	}
+	
+	private static void convertPasswordError(BindingResult result) {
+		for(ObjectError error : result.getGlobalErrors()) {
+			String message = error.getDefaultMessage();
+			if("account.password.mismatch.message".equals(message)) {
+				if(!result.hasFieldErrors("password")) {
+					result.rejectValue("password", "error.mismatch");
+				}	
+			}
+		}
+	}
 }
