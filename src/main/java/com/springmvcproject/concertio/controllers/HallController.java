7f321ec4b377c692d5bdad80f62aee64ae76900b
spaceofmiah <geek_200@outlook.com>
2019-11-12 04:13:13 +0100

    Create request handler for processing Hall creation

--- src/main/java/com/springmvcproject/concertio/controllers/HallController.java
+++ src/main/java/com/springmvcproject/concertio/controllers/HallController.java
@@ -1,13 +1,34 @@
 package com.springmvcproject.concertio.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.springmvcproject.concertio.formbeans.HallCreationForm;
+import com.springmvcproject.concertio.models.Hall;
+import com.springmvcproject.concertio.models.Image;
+import com.springmvcproject.concertio.service.HallService;
+import com.springmvcproject.concertio.service.ImageService;
 
 
 @Controller
 @RequestMapping(value="/hall")
 public class HallController {
+	
+	@Autowired
+	HallService hallService;
+	
+	@Autowired
+	ImageService imageService;
 	
 	@RequestMapping(value = "", method = RequestMethod.GET)
     public String eventListRoute() {
@@ -16,13 +37,48 @@ public class HallController {
 	
 	
     @RequestMapping(value = "book", method = RequestMethod.GET)
-    public String BookEventRoute() {
+    public String BookEventRoute() {    	
         return "bookEvent";
     }
     
+    
     @RequestMapping(value = "create", method = RequestMethod.GET)
-    public String createNewEvent() {
-        return "eventCreation";
+    public String getHallCreationForm(Model model) {
+    	model.addAttribute("hallForm", new HallCreationForm());
+    	return "eventCreation";
+    }
+    
+    @RequestMapping(value="", method= RequestMethod.POST, headers=("content-type=multipart/*"))
+    public String postHallCreationForm(
+    		@ModelAttribute("hallForm") HallCreationForm form,
+    		@RequestParam("file") MultipartFile file,
+    		BindingResult results) {
+    	
+    	// convert object to hall
+    	Hall hall = toHall(form);    	
+    	
+    	// create image from multipart file
+    	Image image = imageService.addNewImage(form.getFile(), results);
+    	
+    	// create a new hall
+    	boolean created = hallService.createNewHall(hall, image, results);
+    	
+    	if (created) {    		
+    		return "redirect:/";
+    	}
+    	
+    	return "eventCreation";
+    }
+    
+    
+    private static Hall toHall(HallCreationForm form) {
+    	Hall newHall = new Hall();
+    	newHall.setName(form.getName());
+    	newHall.setLocation(form.getLocation());
+    	newHall.setPrice(form.getPrice());
+    	newHall.setParkingSpace(form.getParkingSpace());
+    	newHall.setCapactiy(form.getCapacity());
+    	return newHall;
     }
 
 }
